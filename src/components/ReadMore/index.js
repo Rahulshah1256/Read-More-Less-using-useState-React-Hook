db7c4a12c960d1dbeda27d00a09ac9acf658fd60
Rahulshah1256 <nxtwave@ip-10-2-56-207.ap-south-1.compute.internal>
2024-02-01 09:56:48 +0000

    implemented css

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,5 +1,13 @@
 // Write your code here
 import {useState} from 'react'
+import {
+  MainContainer,
+  Title,
+  SubTitle,
+  Image,
+  Description,
+  Button,
+} from './styledComponents'
 
 const ReadMore = props => {
   const {reactHooksDescription} = props
@@ -15,16 +23,16 @@ const ReadMore = props => {
     : reactHooksDescription.slice(0)
 
   return (
-    <div>
-      <h1> React Hooks</h1>
-      <p>Hooks are a new addition to React</p>
-      <img
+    <MainContainer>
+      <Title> React Hooks</Title>
+      <SubTitle>Hooks are a new addition to React</SubTitle>
+      <Image
         src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png"
         alt="react hooks"
       />
-      <p>{text}</p>
-      <button onClick={onChangeRead}>{readMoreHandle}</button>
-    </div>
+      <Description>{text}</Description>
+      <Button onClick={onChangeRead}>{readMoreHandle}</Button>
+    </MainContainer>
   )
 }
 
--- src/components/ReadMore/styledComponents.js
+++ src/components/ReadMore/styledComponents.js
@@ -1,1 +1,73 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`
+
+export const Title = styled.h1`
+  color: #1e293b;
+  font-family: 'Roboto';
+  font-size: 24px;
+  font-weight: 500;
+  margin: 0;
+  line-height: 1.33;
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+    line-height: 1.5;
+  }
+`
+export const SubTitle = styled.p`
+  text-align: center;
+  color: #334155;
+  font-family: 'Roboto';
+  font-size: 14px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  line-height: 1.33;
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    margin-bottom: 16px;
+    line-height: 1.5;
+  }
+`
+export const Image = styled.img`
+  width: 100%;
+  margin-top: 12px;
+  @media screen and (min-width: 768px) {
+    margin-top: 16px;
+  }
+`
+export const Description = styled.p`
+  color: #334155;
+  font-family: 'Roboto';
+  font-size: 14px;
+  margin-top: 16px;
+  margin-bottom: 8px;
+  line-height: 1.7;
+  @media screen and (min-width: 768px) {
+    margin-bottom: 16px;
+  }
+`
+export const Button = styled.button`
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 12px;
+  background-color: #1f81ff;
+  border: none;
+  border-radius: 6px;
+  margin-top: 8px;
+  padding: 8px 16px;
+  align-self: flex-start;
+  cursor: pointer;
+  outline: none;
+  line-height: 1.33;
+  @media screen and (min-width: 768px) {
+    margin-top: 16px;
+    align-self: center;
+  }
+`
